e1218030c24e518fe677aa6cf3585887677b9691
onuryilmaaz <onuryilm.41@gmail.com>
2025-03-04 16:47:09 +0300

    Update AssemblyInformationalVersion and cache for API project

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41244d521157a66364032f734597e66686d6f3c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd5c5607562461494c175bbe51b91902d426c4bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
