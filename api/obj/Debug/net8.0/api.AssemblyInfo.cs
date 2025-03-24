5c9a3004ba83cb2cec09cab7c28d59f94c848357
onuryilmaaz <onuryilm.41@gmail.com>
2025-03-24 09:42:12 +0300

    Translate UI components to Turkish for improved localization

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e397648a2f7996c25348588646367ded609c58c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c9c74d3a9a5ae71f0b2d81c9b2e67f2325a61ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
